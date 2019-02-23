afe19a8d5001f435f86fc2ce002ea9e1231c2111
LucBerge <lucas.bergeron@outlook.fr>
2019-02-23 01:26:56 +0100

    Bug fixed

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+long_description = open("README.md", "r").read()
 
 setuptools.setup(name='Y2T',
       version='1.2',
@@ -13,6 +12,6 @@ setuptools.setup(name='Y2T',
       long_description_content_type="text/markdown",
       url='https://github.com/Esisar-Pro-G/Y2T',
       packages=setuptools.find_packages(),
-      install_requires=['beautifulsoup4 ', 'youtube-dl', 'mutagen'],
+      install_requires=['beautifulsoup4 ', 'youtube-dl', 'mutagen', 'requests'],
       platforms='Linux',
      )
