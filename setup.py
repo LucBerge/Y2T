d5b7542a6e00ceaa178d95f5cdb381d30309a4e6
Lucas <lucas.bergeron@outlook.fr>
2019-07-30 01:21:11 +0200

    Update to 1.4

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import sys
 if sys.version_info < (3, 5):
     sys.exit('Y2T requires Python 3.5+')
@@ -7,14 +5,16 @@ if sys.version_info < (3, 5):
 long_description = open("README.md", "r").read()
 
 import setuptools
-setuptools.setup(name='Y2T',
-      version='1.3',
-      author='Esisar Pro-G',
-      author_email='esisar.pro.g@gmail.com',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/Esisar-Pro-G/Y2T',
-      packages=setuptools.find_packages(),
-      install_requires=['youtube-dl', 'mutagen'],
-      platforms='Linux',
-     )
+setuptools.setup(
+	name='Y2T',
+	version='1.4',
+	author='Esisar Pro-G',
+	author_email='esisar.pro.g@gmail.com',
+	description="Youtube to Torrent",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url='https://github.com/Esisar-Pro-G/Y2T',
+	packages=setuptools.find_packages(),
+	install_requires=['youtube-dl', 'mutagen'],
+	platforms='Linux'
+)
